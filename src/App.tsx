2b4b85badc1cddefecd131c1108c0ed1261331e6
vidyadhar03 <vidyadhariitkgp@gmail.com>
2024-05-28 12:10:12 +0530

    pullinam project init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import { Toolbar } from './Commons/toolbar';
+
+function App() {
+
+  return (
+    <div>
+      <Toolbar/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Commons/toolbar.tsx
@@ -0,0 +1,5 @@
+export function Toolbar() {
+  return <div>
+    <h1>this is toolbar</h1>
+  </div>;
+}
